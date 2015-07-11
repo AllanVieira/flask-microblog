88a846a22ac6ba49dad3ced36188cda4d4dca0d6
AllanVieira <vieira.allanc@gmail.com>
2015-07-11 12:57:46 -0300

    Templates

--- app/views.py
+++ app/views.py
@@ -1,6 +1,22 @@
+# -*- coding: utf-8 -*-
+from flask import render_template
 from app import app 
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello, Word"
+	user = {'login' : 'Allan'}
+	posts = [
+		{
+			'autor' : {'login' : u'joão'},
+			'body' : 'O filme e muito bom'
+		},
+		{
+			'autor' : {'login' : 'maria'},
+			'body' : 'gostei muito do filme'
+		}
+	]
+	return render_template('index.html',
+							title='Home',
+							user=user,
+							posts=posts)
